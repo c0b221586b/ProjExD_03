1b9dc2e6cf7f65ff66364d10ce5aafb348b4a297
Sogo Yokokawa <c0b221586b@edu.teu.ac.jp>
2023-06-13 16:22:52 +0900

    演習２

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     koukaton_img = pg.image.load("ex01/fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img, True, False)
     koukaton_list = [koukaton_img,pg.transform.rotozoom(koukaton_img, 1, 1.0), pg.transform.rotozoom(koukaton_img, 2, 1.0), pg.transform.rotozoom(koukaton_img, 3, 1.0), pg.transform.rotozoom(koukaton_img, 4, 1.0), pg.transform.rotozoom(koukaton_img, 5, 1.0), pg.transform.rotozoom(koukaton_img, 6, 1.0), pg.transform.rotozoom(koukaton_img, 7, 1.0), pg.transform.rotozoom(koukaton_img, 8, 1.0), pg.transform.rotozoom(koukaton_img, 9, 1.0), pg.transform.rotozoom(koukaton_img, 10, 1.0), pg.transform.rotozoom(koukaton_img, 9, 1.0), pg.transform.rotozoom(koukaton_img, 8, 1.0), pg.transform.rotozoom(koukaton_img, 7, 1.0), pg.transform.rotozoom(koukaton_img, 6, 1.0), pg.transform.rotozoom(koukaton_img, 5, 1.0), pg.transform.rotozoom(koukaton_img, 4, 1.0), pg.transform.rotozoom(koukaton_img, 3, 1.0), pg.transform.rotozoom(koukaton_img, 2, 1.0), pg.transform.rotozoom(koukaton_img, 1, 1.0)]
-    
+    gb_img = pg.transform.flip(bg_img,True,False)
     tmr = 0
     x = 0
     while True:
@@ -18,14 +18,16 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0 - x, 0])
-        screen.blit(bg_img, [1600 - x , 0])
+        screen.blit(gb_img,[1600 - x , 0])
+        screen.blit(bg_img, [3200 - x, 0])
         screen.blit(koukaton_list[tmr%19],[300,200])
         
         pg.display.update()
         tmr += 1     
         x += 1
-        if x >= 1600:
+        if x >= 3200:
             x = 0   
+            
         clock.tick(100)
 
 
