fff75cea509f018bfbd473faee0c4630277faf70
Sogo Yokokawa <c0b221586b@edu.teu.ac.jp>
2023-06-27 16:16:21 +0900

    bomb

--- ex03/fight_kokaton.py
+++ ex03/fight_kokaton.py
@@ -136,7 +136,7 @@ def main():
     beam = None
 
     clock = pg.time.Clock()
-    tmr = 0
+    tmr = 0 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
